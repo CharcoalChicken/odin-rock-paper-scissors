3c21a0e714ca92694ea58aa170d5409dc74fd7f0
Amanda T <nazaryounan@Amanda.local>
2025-04-01 18:22:35 +1100

    function that asks human for a input which is either rock, paper or scissors

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 2);
+    let computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice === 0){
         return 'Rock';
@@ -13,6 +13,23 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper or Scissors? ");
+
+    if(humanChoice === 'Rock'){
+        return 'Rock';
+    }
+    else if (humanChoice === 'Paper'){
+        return 'Paper';
+    } 
+    else if (humanChoice === 'Scissors'){
+        return 'Scissors';
+    }
+    else {
+        return 'Invalid answer, choose again.'
+    }
+}
+console.log(getHumanChoice());
 
 
 
