857f1523bd1c843e6db36b279667fc1fb5c779d5
Amanda T <nazaryounan@Amanda.local>
2026-02-01 21:10:26 +1100

    Get Humans Choice of eaither rock, paper or scissors

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+
+
 //Get computers choice
-function getComputersChoice(max) {
+function getComputersChoice(max) {    
     let options = [`Rock`, `Paper`, `Scissors`];
     let random = options[Math.floor(Math.random() * options.length)];
     return random
 }
 getComputersChoice();
-
 console.log(getComputersChoice());
+
+//Get humans choice
+function getHumansChoice() {
+    let humansChoice = prompt("Rock, Paper or Scissors?");
+    return humansChoice
+}
+getHumansChoice();
